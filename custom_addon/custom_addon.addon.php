41f5bf461e0a20e447c7a36076561a324e325d33
1Sam Online <csh@korea.com>
2016-03-16 13:11:27 +0900

    Added files via upload

---
+++ custom_addon/custom_addon.addon.php
@@ -0,0 +1,19 @@
+<?php
+if(!defined('__XE__')) exit();
+/**
+ * @custom_addon.addon.php
+ * @author 1Sam (csh@korea.com)
+ * @brief 1SamOnline 사용자 정의 애드온
+ **/
+
+if($called_position == 'after_module_proc') {
+	
+	//Context::addCSSFile('./addons/custom_addon/css/addon.css');
+	//Context::addJsFile('./addons/custom_addon/js/custom_addon.js', false ,'', null, 'body');
+	//Context::addXmlFile('./addons/custom_addon/js/custom_addon.xml', false ,'', null, 'body');
+	//$document_srl = Context::get('document_srl');
+	//Context::addHtmlFooter($footer_content);
+
+
+
+}
